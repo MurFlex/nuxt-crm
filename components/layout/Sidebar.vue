44a18e99577d6b0c86c44480adcc75ed640919b1
Kirill Mahonin <murflexmur@yandex.ru>
2024-07-20 21:39:30 +0300

    Base layout

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<template>
+	<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+		<NuxtLink class="mb-10 block" to="/">
+			<NuxtImg class="mx-auto" src="/logo.svg" alt="" width="100px" />
+		</NuxtLink>
+
+		<Button> send </Button>
+	</aside>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+	devtools: { enabled: false },
+	modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+	shadcn: {
+		prefix: '',
+		componentDir: './components/ui',
+	},
+})
