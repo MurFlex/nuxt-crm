d99996b697c3ae7e13fd45cb18551662c5811585
Kirill Mahonin <murflexmur@yandex.ru>
2024-08-20 12:27:52 +0300

    card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="p-10">
-		<h1 class="font-bold text-2xl mb-10">CRM System</h1>
-	</div>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">CRM System</h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">Name card</UiCardHeader>
+        <UiCardContent> Компания</UiCardContent>
+        <UiCardFooter> Date</UiCardFooter>
+      </UiCard>
+    </div>
+  </div>
 </template>
